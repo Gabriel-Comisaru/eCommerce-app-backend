0f9b10f1178ab5441f03a5fa09d1405d2dfc4915
Toma <tomaandrei98@gmail.com>
2023-06-20 14:53:48 +0300

    Initial commit

---
+++ src/main/java/com/qual/store/MainProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qual.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainProjectBackendApplication.class, args);
+	}
+
+}
