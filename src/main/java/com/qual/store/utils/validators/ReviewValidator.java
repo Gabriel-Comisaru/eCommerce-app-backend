bfa6d0eea9a3746f71a0bef7930b844ca783e76a
Toma <tomaandrei98@gmail.com>
2023-07-03 18:56:36 +0300

    Update review validator

--- src/main/java/com/qual/store/utils/validators/ReviewValidator.java
+++ src/main/java/com/qual/store/utils/validators/ReviewValidator.java
@@ -2,8 +2,6 @@ package com.qual.store.utils.validators;
 
 import com.qual.store.dto.request.ReviewRequestDto;
 import com.qual.store.exceptions.ValidatorException;
-import com.qual.store.model.Product;
-import com.qual.store.model.Review;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -19,6 +17,8 @@ public class ReviewValidator implements Validator<ReviewRequestDto> {
         conditions.put(Objects::isNull, "review cannot cannot be null");
         conditions.put(rev -> rev != null && rev.getTitle() == null,
                 "review title cannot be null");
+        conditions.put(rev -> rev != null && rev.getTitle() != null && rev.getTitle().trim().isEmpty(),
+                "review title cannot be empty");
         conditions.put(cl -> cl != null && cl.getTitle() != null && cl.getComment().trim().isEmpty(),
                 "review comment cannot be empty");
         conditions.put(cl -> cl != null && cl.getTitle() != null && cl.getComment().trim().isEmpty() && cl.getRating() < 0,
