e31ded4d5b89bcf05976923024352a7ae6e236ab
Toma <tomaandrei98@gmail.com>
2023-07-04 20:04:24 +0300

    Add CrossOrigin for auth controller

--- src/main/java/com/qual/store/security/controller/AuthenticationController.java
+++ src/main/java/com/qual/store/security/controller/AuthenticationController.java
@@ -15,16 +15,14 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin("*")
 public class AuthenticationController {
 
     protected final Log logger = LogFactory.getLog(getClass());
